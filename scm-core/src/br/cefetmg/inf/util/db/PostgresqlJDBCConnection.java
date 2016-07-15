a14dbe03f260c3e79bf35954748f48284c1e2931
igorhenri5 <razrplay5@gmail.com>
2016-07-15 19:46:19 -0300

    BD Driver / Excluir(ebola)

--- scm-core/src/br/cefetmg/inf/util/db/PostgresqlJDBCConnection.java
+++ scm-core/src/br/cefetmg/inf/util/db/PostgresqlJDBCConnection.java
@@ -5,15 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class PostgresqlJDBCConnection implements JDBCConnectionFactory {
-
-    /*private final static String dbDriver = "org.postgresql.Driver";
-    private final static String dbURL = "postgres://svqyjqglpaoby:GTENaP1mrjiCB-wzAwG_pREpA6@ec2-54-235-68-4.compute-1.amazonaws.com:5432/degrcm4gastrfi";
-    private final static String user = "svqygjqglpaoby";
-    private final static String pass = "GTENaP1mrjiCB-wzAwG_pREpA6";
-*/
     
     private final static String dbDriver = "org.postgresql.Driver";
-    private final static String dbURL = "postgres://localhost:5432/postgres";
+    private final static String dbURL = "jdbc:postgresql://localhost:5432/postgres";
     private final static String user = "postgres";
     private final static String pass = "123456";
     
